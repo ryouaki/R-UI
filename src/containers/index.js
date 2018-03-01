389a2544d5fae230b52267e4c374952111c69e41
liangyahui <liangyahui@jingdata.com>
2018-03-01 14:15:21 +0800

    update

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,7 +6,6 @@ import {
 import Bundle from './../components/Bundle';
 import { getPublicUrl } from './../common';
 
-import Header from './Header';
 import Footer from './Footer';
 
 import './../styles/index.css';
@@ -14,7 +13,6 @@ import './../styles/index.css';
 export default class Container extends React.PureComponent {
   render() {
     return <div className="r-ui container">
-      <Header/>
       <Switch>
         <Route exact path={ getPublicUrl('/') } component={ (props) => {
               return <Bundle load={ () => import('./Home') }>
