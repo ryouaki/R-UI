467d3702c08184899cc061d8924b2b55f52dc960
liangyahui <liangyahui@jingdata.com>
2018-03-16 19:56:49 +0800

    no message

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,6 +3,8 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import Bundle from './../components/Bundle';
 import { getPublicUrl } from './../common';
 
@@ -14,13 +16,14 @@ import Footer from './Footer';
 import './../styles/index.css';
 
 export default class Container extends React.Component {
+
   render() {
     return <Layout.Row direction="col" className="container">
       <Layout.Col flex="fixed">
         <Header key="header"/>
       </Layout.Col>
       <Layout.Col flex="growAndShrink">
-        <Switch key="main">
+        <Switch>
           <Route exact path={ getPublicUrl('/') } component={ (props) => {
                 return <Bundle load={ () => import('./Home') }>
                   { Home => <Home {...props} /> }
