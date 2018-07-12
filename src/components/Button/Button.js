82a1a90ad5b9c2b62713c441da2466081e78b245
liangyahui <liangyahui@jingdata.com>
2018-07-12 17:53:51 +0800

    gengxin

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Button extends React.PureComponent {
+  render() {
+    return <button className="button">
+      button
+    </button>
+  }
+};
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
+import Button from './Button';
+
 import './styles/index.css';
 
-export default class Button extends React.PureComponent {
-  render() {
-    return <button className="button">
-      button
-    </button>
-  }
-};
+export default {
+  Button
+}
