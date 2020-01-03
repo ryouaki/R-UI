ea816ca72c170ebe27e2425f36928326e62777f4
Ya Hui Liang(Ryou) <46517115@qq.com>
2020-01-03 14:21:15 +0800

    Update index.js

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -5,7 +5,7 @@ import './../../styles/footer/index.css';
 export default class Footer extends React.PureComponent {
   render() {
     return <footer className="r-ui-footer">
-      @2018 -  Ryouaki(46517115@qq.com) <a href="http://beian.miit.gov.cn或http://www.beian.miit.gov.cn">（京ICP备18044137号）</a>
+      @2018 -  Ryouaki(46517115@qq.com) <a href="http://beian.miit.gov.cn">（京ICP备18044137号）</a>
     </footer>
   }
 }
